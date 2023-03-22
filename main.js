9e3bec68810745523db24f02300056684849d6b6
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-03-21 21:32:36 -0500

    Add jquey to web page

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const label_text = document.querySelector('.label-text')
-const label_result = document.querySelector('.label-result')
-const primary_button = document.querySelector('.primary-button')
-const label_finish = document.querySelector('.label-finish')
-const label_historial = document.querySelector('.label-historial')
+// Jquery
+const label_text = $('.label-text');
+const label_result = $('.label-result');
+const primary_button = $('.primary-button');
+const label_finish = $('.label-finish');
+const label_historial = $('.label-historial');
 
 // Numero random
 const magic = Math.floor(Math.random()*100+1);
@@ -10,27 +11,28 @@ let conteo = 0;
 
 console.log(magic);
 
-primary_button.addEventListener('click', btnOnClick);
+primary_button.on('click', btnOnClick);
 
 function btnOnClick() {
+    console.log('Entro a la funcion');
     if (conteo < 10) {
-    let user = label_result.value;
+    let user = label_result.val();
     
     if (user == magic) {
-        label_finish.innerHTML = "Felicidades, acertastes el numero";
+        label_finish.html("Felicidades, acertastes el numero");
     }
     else if (user < magic) {
-        label_finish.innerHTML = "El numero que has marcado se encuentra por debajo"
+        label_finish.html("El numero que has marcado se encuentra por debajo");
     }
     else if (user > magic) {
-        label_finish.innerHTML = "El numero que has marcado se encuentra por encima"
+        label_finish.html("El numero que has marcado se encuentra por encima");
     }
-    label_historial.append(user + " ")
+    label_historial.append(user + " ");
 
     conteo += 1;
     } 
     else {
         console.log("entro");
-        label_finish.innerHTML = "Has excedido el numero maximo de repeticiones. Vuelve a intetarlo"
+        label_finish.html("Has excedido el numero maximo de repeticiones. Vuelve a intetarlo");
     }
 }
